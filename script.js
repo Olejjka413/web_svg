b3174a7d82735c3df10d3719e93c34ab504a9275
Olejjka413 <55927593+Olejjka413@users.noreply.github.com>
2021-07-11 16:37:34 +0300

    анимация лампочки

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const burger = document.querySelector('.burger');
-const lightOn = document.getElementById('.switch');
-const bulb = document.getElementById('.light');
 
 const handleBurger = () => {
     burger.classList.toggle('burger__active');
 };
 
-const handleLightOn = () => {
-    bulb.classList.toggle('light--on');
-    lightOn.classList.toggle('switch--on');
-};
-
 burger.addEventListener('click', handleBurger);
-lightOn.addEventListener('click', handleLightOn);
+
+
+$(document).ready(function() {
+    $('#switch').click(function () {
+        $('.bulb').toggleClass('bulb--on');
+        $('.star').toggleClass('star--on');
+        
+    })
+});
